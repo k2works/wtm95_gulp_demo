3acb66040c60716fb0caf168f1416cb62cba5989
takanashi66 <takanashi66@gmail.com>
2016-11-12 23:46:44 +0900

    修正

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ var gulp					= require('gulp');
 var sass					= require('gulp-sass');
 var autoprefixer	= require('gulp-autoprefixer');
 var plumber				= require('gulp-plumber');
-var sourcemaps 		= require('gulp-sourcemaps');
-var browserSync 	= require('browser-sync').create();
+var sourcemaps		= require('gulp-sourcemaps');
+var browserSync		= require('browser-sync').create();
 
 gulp.task('browser-sync', function() {
 	browserSync.init({
